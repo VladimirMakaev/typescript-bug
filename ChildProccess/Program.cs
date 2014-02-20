ecb127bc629e9d20a4334a7c8cc5f901880e5e6d
Vladimir Makaev <c_vmakaev@anybill.com>
2014-02-20 12:58:41 +0300

    Create Repo

---
+++ ChildProccess/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ChildProccess
+{
+    using System;
+    using System.Reflection;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Type t =
+                    Type.GetType(
+                        "TypeScriptLanguageService.CompilerServices.FileHelpers, TypeScriptLanguageService, Version=0.9.5.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
+                MethodInfo typeScriptServicesPath = t.GetMethod(
+                    "GetSetupLocation",
+                    BindingFlags.Static | BindingFlags.NonPublic);
+                Console.WriteLine(typeScriptServicesPath.Invoke(null, null));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ TypeScriptServices/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeScriptServices
+{
+    using System.IO;
+
+    static class Program
+    {
+        [LoaderOptimization(LoaderOptimization.MultiDomainHost)]
+        [STAThread]
+        static void Main()
+        {
+            string startupPath = Path.GetDirectoryName(@"c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\");
+            string configFile = Path.Combine(startupPath, "ChildProccess.exe.config");
+            string assembly = Path.Combine(startupPath, "ChildProccess.exe");
+
+            // Create the setup for the new domain:
+            AppDomainSetup setup = new AppDomainSetup
+                                       {
+                                           ApplicationName = "ChildProcess",
+                                           ShadowCopyFiles = "true",
+                                           ConfigurationFile = configFile,
+                                           ApplicationBase = startupPath
+                                       };
+
+            // Create the application domain. The evidence of this
+            // running assembly is used for the new domain:
+            AppDomain domain = AppDomain.CreateDomain(
+                "ChildProcess",
+                AppDomain.CurrentDomain.Evidence,
+                setup);
+
+            // Start MyApplication by executing the assembly:
+            domain.ExecuteAssembly(assembly);
+
+            // After the MyApplication has finished clean up:
+            AppDomain.Unload(domain);
+        }
+    }
+}
